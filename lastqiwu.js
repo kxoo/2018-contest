629e11f29dae40cf09bdef6371c26d260eeac16c
kxoo <apple@ZKB-MacBook-Pro.local>
2018-04-17 23:02:39 +0800

    Version 0.2

--- lastqiwu.js
+++ lastqiwu.js
@@ -1,91 +1,110 @@
 
 var canvas = document.createElement('canvas');
 var ctx=canvas.getContext('2d');
-canvas.width = 200;
-canvas.height = 200;
+canvas.width = 400;
+canvas.height = 400;
+ctx.font="60px Arial";
+ctx.strokeStyle= "rgb(0,0,0)";
 document.body.appendChild(canvas);
 
-var arr = [2, 4, 8];
-var start = parseInt(math.random() * 2);
-var text = [];
-text[0] = arr[start];
-function startNum() {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 0);
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, 200);
-    ctx.moveTo(200, 0);
-    ctx.lineTo(200, 200);
-    ctx.moveTo(0, 200);
-    ctx.lineTo(200, 200);//外框
-    ctx.moveTo(50, 0);
-    ctx.lineTo(50, 200);
-    ctx.moveTo(100, 0);
-    ctx.lineTo(100, 200);
-    ctx.moveTo(150, 0);
-    ctx.lineTo(150, 200); //三条横线
-    ctx.moveTo(0, 50);
-    ctx.lineTo(200, 50);
-    ctx.moveTo(0, 100);
-    ctx.lineTo(200, 100);
-    ctx.moveTo(0, 150);
-    ctx.lineTo(200, 150);//三条竖线
-    ctx.stroke();
-    window.requestAnimationFrame(starGame);
+var arr = [2, 2, 4];
+var text = {
+    put: [],
+    y: 70,
+    x: 30
+};
+
+function sum(order) {
+	for(var i = 0; i < 16 ;i++) {
+		if(text.put[i] == text.put[i + order] && text.put[i] && (i - 3) % 4) {
+                text.put[i] === '';
+                text.put[i + order] = 2 * text.put[i];
+                
+            }
+	}
 }
 
+
+function init(i) {
+	if(!text.put[i] && !log) {
+		left = 1;
+	    text.put[i] = arr[parseInt(Math.random() * 3)];
+	}
+}
+(function startNum() {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(400, 0);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, 400);
+    ctx.moveTo(400, 0);
+    ctx.lineTo(400, 400);
+    ctx.moveTo(0, 400);
+    ctx.lineTo(400, 400);//外框
+    ctx.moveTo(100, 0);
+    ctx.lineTo(100, 400);
+    ctx.moveTo(200, 0);
+    ctx.lineTo(200, 400);
+    ctx.moveTo(300, 0);
+    ctx.lineTo(300, 400); //三条横线
+    ctx.moveTo(0, 100);
+    ctx.lineTo(400, 100);
+    ctx.moveTo(0, 200);
+    ctx.lineTo(400, 200);
+    ctx.moveTo(0, 300);
+    ctx.lineTo(400, 300);//三条竖线
+    ctx.closePath();
+    ctx.stroke();
+
+    for(var i = 0; i <= 15; i ++) {
+    	// text.put[i] = arr[start];	//test
+    	if (text.put[i] === undefined) {
+    		text.put[i] = '';
+    	}
+        ctx.fillText(text.put[i], text.x, text.y);
+
+    	if((i - 3) % 4 === 0 ) {
+    		text.y += 100;
+    		text.x = 30;
+    	}else {
+    		text.x += 100;
+    	}
+    	
+    }
+    text.y = 20;
+    window.requestAnimationFrame(startNum);
+})();
+
+'/'
 addEventListener('keydown', function (e) {
     if (e.keyCode === 37) {//left
+    	var log = 0;
         for(var i = 0; i <= 12; i += 4) {
-            if(text[i] == text[i + 4]) {
-                text[i] === null;
-                text[i + 4] = 2 * text[i];
-
-            }
-            if(text[i] == null) {
-                text[i] = arr[start];
-                break;
-            }
+            init(i);
         }
+        sum(1);
+        log = 0;
     }else
     if (e.keyCode === 38) {//up
         for(var i = 0; i <= 3; i++) {
-            if(text[i] == text[i + 1]) {
-                text[i] === null;
-                text[i + 1] = 2 * text[i];
-
-            }
-            if(text[i] == null) {
-                text[i] = arr[start];
-                break;
-            }
-        }
+            init(i);
+	    }
+        sum(4);
+        log = 0;
     }else
     if (e.keyCode === 39) {//right
         for(var i = 3; i <= 15; i+=4) {
-            if(text[i] == text[i + 4]) {
-                text[i] === null;
-                text[i + 4] = 2 * text[i];
-            }
-            if(text[i] == null) {
-                text[i] = arr[start];
-                break;
-            }
+            init(i);
         }
+        sum(1);        
+        log = 0;
     }else
     if (e.keyCode === 40) {//down
         for(var i = 12; i <= 15; i++) {
-            if(text[i] == text[i + 1]) {
-                text[i] === null;
-                text[i + 1] = 2 * text[i];
-
-            }
-            if(text[i] == null) {
-                text[i] = arr[start];
-                break;
-            }
+            init(i);
         }
+        sum(4);
+        log = 0;
     }
     
 }, false);
