199701c779e8d7de886e8592c721c834c6d0ce11
kxoo <apple@ZKB-MacBook-Pro.local>
2018-04-18 19:51:46 +0800

    Version 0.4

--- lastqiwu.js
+++ lastqiwu.js
@@ -3,6 +3,8 @@ var canvas = document.createElement('canvas');
 var ctx=canvas.getContext('2d');
 canvas.width = 400;
 canvas.height = 400;
+ctx.fillStyle="#373c38";
+ctx.fillRect(0, 0, 400, 400);
 ctx.font="60px Arial";
 ctx.strokeStyle= "rgb(0,0,0)";
 document.body.appendChild(canvas);
@@ -16,39 +18,38 @@ var text = {
 
 function sum(order) {
 	for(var i = 0; i < 16 ;i++) {
-		if(text.put[i] == text.put[i + order] && text.put[i] && (i - 3) % 4) {
-                text.put[i] = '';
+		if(text.put[i] == text.put[i + order] && text.put[i]) {
                 text.put[i + order] = 2 * text.put[i];
-                
+                text.put[i] = '';                
             }else 
-              if(!text.put[i + order] && text.put[i] && (i - 3) % 4) {
-                text.put[i] = '';
+              if(!text.put[i + order] && text.put[i] ) {
                 text.put[i + order] =  text.put[i];
+                text.put[i] = '';
               }
 	}
 }
 
 function reduce(order) {
 	for(var i = 0; i < 16 ;i++) {
-		if(text.put[i] == text.put[i - order] && text.put[i] && (i - 3) % 4) {
-                text.put[i] = '';
+		if(text.put[i] == text.put[i - order] && text.put[i]) {
                 text.put[i - order] = 2 * text.put[i];
-                
+                text.put[i] = '';                
             }else 
-              if(!text.put[i - order] && text.put[i] && (i - 3) % 4) {
-                text.put[i] = '';
+              if(!text.put[i - order] && text.put[i]) {               
                 text.put[i - order] =  text.put[i];
+				text.put[i] = '';
               }
 	}
 }
 
 function init(i, log) {
 	if(!text.put[i] && !log) {
+		log = 1;
 	    text.put[i] = arr[parseInt(Math.random() * 3)];
-      return log = 1;
 	}
 }
-function startNum() {
+
+(function startNum() {
     ctx.beginPath();
     ctx.moveTo(0, 0);
     ctx.lineTo(400, 0);
@@ -74,7 +75,6 @@ function startNum() {
     ctx.stroke();
 
     for(var i = 0; i <= 15; i ++) {
-    	// text.put[i] = arr[start];	//test
     	if (text.put[i] === undefined) {
     		text.put[i] = '';
     	}
@@ -86,47 +86,48 @@ function startNum() {
     	}else {
     		text.x += 100;
     	}
+    	 if(text.x > 350 || text.y > 400) {
+    	 	text.put[i] = '';
+    	 	
+    	 }
     	
     }
     text.y = 70;
-  window.requestAnimationFrame(startNum);
-
-
-};
+  	window.requestAnimationFrame(startNum);
+})();
 
 addEventListener('keydown', function (e) {
     var log = 0;
     if (e.keyCode === 37) {//left
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        for(var i = 0; i <= 12; i += 4) {
-            init(i);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        reduce(1);
+        for(var i = 3; i <= 15; i += 4) {
+            init(i, log);
         }
-        sum(1);
-      startNum();
         log = 0;
     }else
     if (e.keyCode === 38) {//up
-        for(var i = 0; i <= 3; i++) {
-            ctx.clearRect(0,0,canvas.width,canvas.height);
+        reduce(4);    	
+        for(var i = 12; i <= 15; i++) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             init(i);
 	    }
-        sum(4);
         log = 0;
     }else
     if (e.keyCode === 39) {//right
-        for(var i = 3; i <= 15; i+=4) {
-            ctx.clearRect(0,0,canvas.width,canvas.height);
+    	sum(1);        
+        for(var i = 0; i <= 12; i += 4) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             init(i);
         }
-        reduce(1);        
         log = 0;
     }else
     if (e.keyCode === 40) {//down
-        for(var i = 12; i <= 15; i++) {
-            ctx.clearRect(0,0,canvas.width,canvas.height);
+        sum(4);    	
+        for(var i = 0; i <= 13; i++) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             init(i);
         }
-        reduce(4);
         log = 0;
     }
     
